c103b9d46bc1a1699ea5871d5385d6df99e88fdc
mmrakt <mimuraakkun3@gmail.com>
2022-06-19 13:01:17 +0900

    Add toast notify

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,3 +1,4 @@
+import { useToast, UseToastOptions } from '@chakra-ui/react'
 import { User } from 'next-auth'
 import { useSession } from 'next-auth/react'
 import Image from 'next/image'
@@ -34,6 +35,7 @@ function Settings(): React.ReactElement {
   })
   const queryClient = useQueryClient()
   const router = useRouter()
+  const toast = useToast()
 
   React.useEffect(() => {
     setValue('name', session?.user.name)
@@ -59,8 +61,22 @@ function Settings(): React.ReactElement {
     try {
       await updateUserInfoMutate({ name, profile })
       await updateUserInfoOnLocalStorage({ name, profile })
+      toast({
+        title: 'Success!',
+        position: 'top-left',
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      })
     } catch (error) {
       console.error(error)
+      toast({
+        title: 'Error',
+        position: 'top-left',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
     } finally {
       router.push('/settings')
     }
