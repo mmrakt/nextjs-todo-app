e82231ce2a49c073e54e0e87cc7efa0daa2e08d7
mmrakt <mimuraakkun3@gmail.com>
2022-07-18 19:14:56 +0900

    Remove

--- src/hooks/todo/useDeleteCompletedTodo.ts
+++
@@ -1,9 +0,0 @@
-// import { useDeleteTodo } from '@/hooks/todo'
-// import { Todo } from '@/libs/prisma'
-
-// const useDeleteCompletedTodo = (todo: Todo) => {
-//     useDeleteTodo.mutate({
-//         todo,
-//         'completedTodos'
-//   })
-// }
